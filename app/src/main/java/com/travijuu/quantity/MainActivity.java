1d62b837f73c9575537d222c4f6fd97132754b79
Erkin Çakar <travijuu@gmail.com>
2016-05-26 21:42:31 +0300

    limitExceededListener added

--- app/src/main/java/com/travijuu/quantity/MainActivity.java
+++ app/src/main/java/com/travijuu/quantity/MainActivity.java
@@ -2,8 +2,6 @@ package com.travijuu.quantity;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
-import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
