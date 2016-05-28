f281d59dc3ef030d632b58ff09648825dc6f3d94
Erkin Çakar <erkin@positive.com.tr>
2016-05-29 00:18:17 +0300

    gradle settings

--- sample/src/main/java/com/travijuu/numberpicker/sample/MainActivity.java
+++ sample/src/main/java/com/travijuu/numberpicker/sample/MainActivity.java
@@ -1,11 +1,11 @@
 package com.travijuu.numberpicker.sample;
 
-import android.support.v7.app.AppCompatActivity;
+import android.app.Activity;
 import android.os.Bundle;
 
 import com.travijuu.numberpicker.library.NumberPicker;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
