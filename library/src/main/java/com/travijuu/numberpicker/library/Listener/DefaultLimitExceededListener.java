162b30885522a6ba3c64857a25b650dfbe82e743
Erkin Çakar <travijuu@gmail.com>
2016-05-29 01:46:09 +0300

    refactor

--- library/src/main/java/com/travijuu/numberpicker/library/Listener/DefaultLimitExceededListener.java
+++ library/src/main/java/com/travijuu/numberpicker/library/Listener/DefaultLimitExceededListener.java
@@ -2,10 +2,12 @@ package com.travijuu.numberpicker.library.Listener;
 
 import android.util.Log;
 
+import com.travijuu.numberpicker.library.Interface.LimitExceededListener;
+
 /**
  * Created by travijuu on 26/05/16.
  */
-public class DefaultLimitExceededListener implements com.travijuu.numberpicker.library.Interface.LimitExceededListener {
+public class DefaultLimitExceededListener implements LimitExceededListener {
 
     public void limitExceeded(int limit, int exceededValue) {
 
